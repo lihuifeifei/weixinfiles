cdc5b31117e13bfeb3e81a156b76d575258c3c04
wangle <wangle@nibirutech.com>
2019-01-24 19:38:17 +0800

    lihui

--- pages/member/index/index.js
+++ pages/member/index/index.js
@@ -1,4 +1,5 @@
 // pages/member/index/index.js
+var app = getApp();
 Page({
 
   /**
@@ -6,7 +7,7 @@ Page({
    */
   data: {
     "avatar":"/images/avatar.jpg",
-    "username":"登录"
+    "username":"您还未登录"
   },
   //点击关于我们跳转到aboutus页面
   aboutUs:function(){
@@ -14,17 +15,87 @@ Page({
       url: '/pages/member/aboutus/aboutus',
     })
   },
-  //点击登录跳转到授权页面
-  login:function(){
-    wx.navigateTo({
-      url: '/pages/login/login',
+
+  bindGetUserInfo: function (e) {
+    if (e.detail.userInfo) {
+      //用户按了一键登录按钮
+      var that = this;
+      //插入登录的用户的相关信息到数据库
+      wx.request({
+        url: app.globalData.prefix_url + '/login/getSession',
+        data: {
+          openid: getApp().globalData.openid,
+          nickName: e.detail.userInfo.nickName,
+          avatarUrl: e.detail.userInfo.avatarUrl,
+          province: e.detail.userInfo.province,
+          city: e.detail.userInfo.city
+        },
+        header: {
+          'content-type': 'application/json'
+        },
+        success: function (res) {
+          //从数据库获取用户信息
+          that.queryUsreInfo();
+          console.log("插入小程序登录用户信息成功！");
+        }
+      });
+      //授权成功后，跳转进入小程序首页
+      wx.switchTab({
+        url: '/pages/member/index/index'
+      })
+    } else {
+      //用户按了拒绝按钮
+      wx.showModal({
+        title: '警告',
+        content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
+        showCancel: false,
+        confirmText: '返回授权',
+        success: function (res) {
+          if (res.confirm) {
+            console.log('用户点击了“返回授权”')
+          }
+        }
+      })
+    }
+  },
+  queryUsreInfo: function () {
+    wx.request({
+      url: app.globalData.prefix_url + '/login/getSession',
+      data: {
+        openid: getApp().globalData.openid
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data);
+        getApp().globalData.userInfo = res.data;
+      }
     })
   },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this;
+    // 查看是否授权
+    wx.getSetting({
+      success: function (res) {
+        if (res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: function (res) {
+              //从数据库获取用户信息
+              that.queryUsreInfo();
+              //用户已经授权过
+              wx.switchTab({
+                url: '/pages/member/index/index'
+              })
+            }
+          });
+        }
+      }
+    })
   },
 
   /**
